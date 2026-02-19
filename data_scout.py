76fd4ffc4fe368d1ddb731d713139bcb1ac407ff
lihaoyu0311 <2878242722@qq.com>
2026-02-19 21:46:13 +0800

    Initialize data_scout.py with data loading logic
    Fix import statement and correct method calls for reading CSV.

---
+++ data_scout.py
@@ -0,0 +1,9 @@
+imporrt pandas as pd
+#加载我们的情报（数据集）
+df = pd.read.csv(cybersecurity_intrucsion_data.csv)
+#执行侦察：看看数据规模
+print("[情报信息]")
+print(f"数据总行数（记录数）：{df.shape[0]}")
+print(f"数据中总列数（特征数）：{df.shape[1]}")\
+print("\n前几条记录预览：")
+print(df.head())
